3b168472fdcd5e7bae90f562b192b115712674d2
Bertrand Kennedy Black <bblack@pbio381.uvm.edu>
2020-01-29 11:37:17 -0500

    trimmed seq for GFM

---
+++ myscripts/fastqc.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+cd ~/Ecological-Genomics-2020/myresults/
+
+# I am creating a new dir to store my results
+
+
+mkdir fastqc
+
+for file in /data/project_data/RS_ExomeSeq/fastq/edge_fastq/GFM*fastq.gz
+
+do
+
+
+fastqc ${file} -o fastqc/
+
+done
+
+
+
