3b3c33a43e0312d2bd4baa4b442b34ded415a0ac
bblack2019 <59922283+bblack2019@users.noreply.github.com>
2020-02-25 08:36:22 -0500

    Diversity Stats

--- myscripts/ANGSD_mypop.sh
+++ myscripts/ANGSD_mypop.sh
@@ -2,7 +2,7 @@
 
 myrepo="/users/b/b/bblack/Ecological-Genomics-2020"
 
-mkdir ${myrepo}/myresults/ANGSD
+#mkdir ${myrepo}/myresults/ANGSD
 
 output="${myrepo}/myresults/ANGSD"
 
@@ -33,3 +33,64 @@ ANGSD -b ${output}/${mypop}_bam.list \
 -doSaf 1 \
 -doHWE 1 \
 # -SNP_pval 1e-6
+
+# Now lets calcualte the SFS for the folded minor allele spectra:
+
+ANGSD -b ${output}/${mypop}_bam.list \
+-ref ${REF} -anc ${REF} \
+-out ${output}/${mypop}_folded_allsites \
+-nThreads 1 \
+-remove_bads 1 \
+-C 50 \
+-baq 1 \
+-minMapQ 20 \
+-minQ 20 \
+-setMinDepth 3 \
+-minInd 2 \
+-setMinDepthInd 1 \
+-setMaxDepthInd 17 \
+-skipTriallelic 1 \
+-GL 1 \
+-doCounts 1 \
+-doMajorMinor 1 \
+-doMaf 1 \
+-doSaf 1 \
+-fold 1 
+
+# Get a rough first est of SFS and then use as a prior for the next estimate 
+
+realSFS ${output}/${mypop}_folded_allsites.saf.idx \
+-maxIter 1000 -tole 1e-6 -P 1 \
+> ${output}/${mypop}_outFold.sfs
+
+# Get refined est of the SFS and doTheta  
+
+ANGSD -b ${output}/${mypop}_bam.list \
+-ref ${REF} -anc ${REF} \
+-out ${output}/${mypop}_folded_allsites \
+-nThreads 1 \
+-remove_bads 1 \
+-C 50 \
+-baq 1 \
+-minMapQ 20 \
+-minQ 20 \
+-setMinDepth 3 \
+-minInd 2 \
+-setMinDepthInd 1 \
+-setMaxDepthInd 17 \
+-skipTriallelic 1 \
+-GL 1 \
+-doCounts 1 \
+-doMajorMinor 1 \
+-doMaf 1 \
+-doSaf 1 \
+-fold 1 \
+-pest ${output}/${mypop}_outFold.sfs \
+-doThetas 1 
+
+# Use the doThetas output to est nucleotide diveristy 
+
+thetaStat do_stat ${output}/${mypop}_folded_allsites.thetas.idx
+
+
+
